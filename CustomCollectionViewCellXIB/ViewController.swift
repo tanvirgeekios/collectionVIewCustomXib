a70cc5a6ddcc15803b80bc924b39f4b621a1671e
tanvirgeekios <tanvir.alam@iosventure.com>
2021-02-22 12:00:16 +0600

    Initial Commit

---
+++ CustomCollectionViewCellXIB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomCollectionViewCellXIB
+//
+//  Created by MD Tanvir Alam on 22/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
